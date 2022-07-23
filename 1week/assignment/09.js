473c5a531b87795e54633cb704aeefbbfb340f3c
Your Name <wjdgml3834@naver.com>
2022-07-23 10:05:18 +0900

    [1주차과제]-09번2차풀이

--- 1week/assignment/09.js
+++ 1week/assignment/09.js
@@ -1,27 +1,6 @@
 /**
  * 리팩토링 해주세요!
  */
-// const SUMMER_START = 1561075200;
-// const SUMMER_END = 1569196800;
-// const SUMMER_RATE = 10;
-// const WINTER_RATE = 20;
-// const WINTER_SERVICE_CHARGE = 30;
-
-// const before = (date, limit) => date < limit;
-// const after = (date, limit) => limit < date;
-
-// const getCharge = (quantity, date) => {
-//   if (before(date, SUMMER_START) || after(date, SUMMER_END)) {
-//     return quantity * WINTER_RATE + WINTER_SERVICE_CHARGE;
-//   }
-//   else {
-//     return quantity * SUMMER_RATE;
-//   }
-// }
-
-// const charge = getCharge(3, new Date().getTime());
-// console.log(charge);
-
 class Charge {
   constructor(
     SUMMER_START,
@@ -47,8 +26,8 @@ class Charge {
   }
 }
 
-const charge = new Charge("1561075200", "1569196800", "10", "20", "30");
+const charge = new Charge(1561075200, 1569196800, 10, 20, 30);
 
-let result = charge.getCharge(3, new Date().getTime());
+const result = charge.getCharge(3, new Date().getTime());
 
 console.log(result);
