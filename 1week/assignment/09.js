b161604a7de911ec34ac7a12503a80e39638d105
Your Name <wjdgml3834@naver.com>
2022-07-23 10:11:41 +0900

    [1주차과제]-09번주석추가

--- 1week/assignment/09.js
+++ 1week/assignment/09.js
@@ -1,6 +1,7 @@
 /**
  * 리팩토링 해주세요!
  */
+// 따로따로 흩어져있는 변수들과 함수를 엮어주기위해 Charge라는 이름으로 클래스를 만들어줌.
 class Charge {
   constructor(
     SUMMER_START,
@@ -25,9 +26,9 @@ class Charge {
     }
   }
 }
-
+// Charge의 인스턴스를 새로 하나 선언해줌.
 const charge = new Charge(1561075200, 1569196800, 10, 20, 30);
-
+// 최종 결과물을 담은 result 변수 선언 및 할당.
 const result = charge.getCharge(3, new Date().getTime());
 
 console.log(result);
